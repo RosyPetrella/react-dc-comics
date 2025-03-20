169e2480540e3def4dd3e4550a8451a0f0fe7b09
RosyPetrella <petrellamr@gmail.com>
2025-03-20 14:47:08 +0100

    Add initial components and assets for the DC Comics project

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+export default function Header() {
+
+    return(
+            <div className="container">
+                <div>
+                   <img src="/dc-comics-1/dc-logo.png" alt="" />
+                <h3>logo</h3> 
+                </div>
+                <div>
+                 <ul>
+                    <li><a href="">CHARACTERS</a></li>
+                    <li><a href="">COMICS</a></li>
+                    <li><a href="">MOVIES</a></li>
+                    <li><a href="">TV</a></li>
+                    <li><a href="">GAMES</a></li>
+                    <li><a href="">COLLECTIBLES</a></li>
+                    <li><a href="">VIDEOS</a></li>
+                    <li><a href="">FANS</a></li>
+                    <li><a href="">NEWS</a></li>
+                    <li><a href="">SHOP</a></li>
+                </ul>
+            </div>    
+        </div>
+    )
+
+}
