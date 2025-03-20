d0cb88bf2135cc99f5fcf437023b7100206d78c1
RosyPetrella <petrellamr@gmail.com>
2025-03-20 17:28:55 +0100

    Add new footer images and restructure Footer component for improved layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,68 +4,71 @@ export default function Footer() {
         <>
         <section>
             <div className="container footer-links">
-            <nav>
-                    <ul>
-                        <h4>DC COMICS</h4>
-                        <li><a href="#">Characters</a></li>
-                        <li><a href="#">Comics</a></li>
-                        <li><a href="#">Movies</a></li>
-                        <li><a href="#">TV</a></li>
-                        <li><a href="#">Games</a></li>
-                        <li><a href="#">Videos</a></li>
-                        <li><a href="#">News</a></li>
-                    </ul>
-                </nav>
-                <div>
-                    <ul>
-                        <h4>SHOP</h4>
-                        <li><a href="#">Shop DC</a></li>
-                        <li><a href="#">Shop DC Collectibles</a></li>
-                    </ul>
+                <div className="col-4">
+                        <div>
+                          <ul>
+                            <h4>DC COMICS</h4>
+                            <li><a href="#">Characters</a></li>
+                            <li><a href="#">Comics</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">TV</a></li>
+                            <li><a href="#">Games</a></li>
+                            <li><a href="#">Videos</a></li>
+                            <li><a href="#">News</a></li>
+                        </ul> 
+                            <ul>
+                                <h4>SHOP</h4>
+                                <li><a href="#">Shop DC</a></li>
+                                <li><a href="#">Shop DC Collectibles</a></li>
+                            </ul>
+                        </div>
+                    <div>
+                        <ul>
+                        <h4>DC</h4>
+                            <li><a href="#">Terms Of Use</a></li>
+                            <li><a href="#">Privacy Policy</a></li>
+                            <li><a href="#">Ad Choices</a></li>
+                            <li><a href="#">Advertising</a></li>
+                            <li><a href="#">Jobs</a></li>
+                            <li><a href="#">Subscriptions</a></li>
+                            <li><a href="#">Talent Workshops</a></li>
+                            <li><a href="#">Ratings</a></li>
+                            <li><a href="#">Shop Help</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                        </ul>
+                    </div>
+                    <div>
+                        <ul>
+                        <h4>SITES</h4>
+                            <li><a href="#">DC</a></li>
+                            <li><a href="#">MAD Magazine</a></li>
+                            <li><a href="#">DC Kids</a></li>
+                            <li><a href="#">DC Universe</a></li>
+                            <li><a href="#">DC Power Visa</a></li>
+                        </ul>
+                    </div>
+                        
                 </div>
-                <div>
-                    <ul>
-                    <h4>DC</h4>
-                        <li><a href="#">Terms Of Use</a></li>
-                        <li><a href="#">Privacy Policy</a></li>
-                        <li><a href="#">Ad Choices</a></li>
-                        <li><a href="#">Advertising</a></li>
-                        <li><a href="#">Jobs</a></li>
-                        <li><a href="#">Subscriptions</a></li>
-                        <li><a href="#">Talent Workshops</a></li>
-                        <li><a href="#">Ratings</a></li>
-                        <li><a href="#">Shop Help</a></li>
-                        <li><a href="#">Contact Us</a></li>
-                    </ul>
+
+                <div className="col-8">
+
                 </div>
-                <nav>
-                    <ul>
-                      <h4>SITES</h4>
-                        <li><a href="#">DC</a></li>
-                        <li><a href="#">MAD Magazine</a></li>
-                        <li><a href="#">DC Kids</a></li>
-                        <li><a href="#">DC Universe</a></li>
-                        <li><a href="#">DC Power Visa</a></li>
-                    </ul>
-                </nav>
+            
             </div>
         </section>
         <section>
             <div class="container bottom">
-                <div className="col-3">
+    
                     <button class="signup-btn">SIGN-UP NOW!</button>
                 <div class="social-links">
                     <span>FOLLOW US</span>
-                    <a href="#"><img src="" alt="Facebook"/></a>
-                    <a href="#"><img src="" alt="Twitter"/></a>
-                    <a href="#"><img src="" alt="YouTube"/></a>
-                    <a href="#"><img src="" alt="Pinterest"/></a>
-                    <a href="#"><img src="" alt="Location"/></a>
+                    <a href="#"><img src="/footer-facebook.png" alt="Facebook"/></a>
+                    <a href="#"><img src="/footer-twitter.png" alt="Twitter"/></a>
+                    <a href="#"><img src="/footer-youtube.png" alt="YouTube"/></a>
+                    <a href="#"><img src="/footer-pinterest.png" alt="Pinterest"/></a>
+                    <a href="#"><img src="/footer-periscope.png" alt="Location"/></a>
                 </div>
-                </div>
-                <div className="col-9">
-
-                </div>
+                
                 
             </div>
         </section>
